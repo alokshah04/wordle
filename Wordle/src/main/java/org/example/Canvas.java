d471aa6839d928eaaa91bf3a82fac413cc6cc8c9
alokshah04 <45050398+alokshah04@users.noreply.github.com>
2023-01-26 00:22:19 -0500

    Add files via upload

---
+++ Wordle/src/main/java/org/example/Canvas.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Canvas extends JComponent {
+
+        public static void main(String[] args) {
+            System.out.println("Hallo");
+        }
+}
